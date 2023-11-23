23b6db768a659cca271a7b72bdb9ecdcce1560de
Lukas Karsky <79151996+Menfiss@users.noreply.github.com>
2023-11-23 09:10:21 +0100

    done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Diary
 {
@@ -62,8 +52,8 @@ namespace Diary
                 {
                     string text = TextForPage.Text;
                     DateOnly date = DateOnly.FromDateTime((DateTime)Calendar.SelectedDate);
-                
-                    diary.Add(new Page(text,date));
+                    Page page = new Page(text, date);
+                    diary.Add(page);
                     UpdatePage();
                     TextForPage.Text = "";
                     Calendar.SelectedDate = DateTime.Today;
--- Page.cs
+++ Page.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Diary
 {
-    public readonly struct Page
+    
+    public struct Page
     {
-        public readonly string Text;
-        public readonly DateOnly Date;
+        public string Text;
+        public DateOnly Date;
 
         public Page(string Text, DateOnly Date) 
         {
